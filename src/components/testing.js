725d57ab6b575a1b8d34580656e3efff03c2ea29
Craig Tuttle <Craigtut@gmail.com>
2017-12-11 16:05:35 -0800

    testing

--- src/components/testing.js
+++ src/components/testing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Testing extends Component {
   render() {
     return (
-      <div>
+      <div id="testing">
         Hello World
       </div>
     );
